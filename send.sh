e022e12b76f73b20179cfa386fc30c599aa77cc1
Yon Ploj <yon.ploj@gmail.com>
2021-03-18 15:35:24 +0100

    pozabu sm kaj use sm spremenu lp

--- send.sh
+++ send.sh
@@ -1,20 +1,90 @@
+#!/bin/bash
 #? Send a message via shellmail
 #? usage: echo "address message" | send.sh
 #? depends: none
-#! ignores all input except the first line
-#! the address mustn't contain spaces
+#! the message is automatically b64 encoded and encrypted with the recipient's address
 
-# reads from stdin if called without arguments
-# otherwise attempts to interpret the argument as an input file
+# The following code was sponsored by StackOverflow
+# https://stackoverflow.com/a/29754866
+# Thank you Robert Siemer for your submission
 
-# the message is automatically b64 encoded and encrypted with the recipient's address
-if read line
+# More safety, by turning some bugs into errors.
+# Without `errexit` you don’t need ! and can replace
+# PIPESTATUS with a simple $?, but I don’t do that.
+set -o errexit -o pipefail -o noclobber -o nounset
+
+# -allow a command to fail with !’s side effect on errexit
+# -use return value from ${PIPESTATUS[0]}, because ! hosed $?
+! getopt --test > /dev/null 
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+	echo 'I’m sorry, `getopt --test` failed in this environment.'
+	exit 1
+fi
+
+OPTIONS=m:a:
+LONGOPTS=message:,address:
+
+# -regarding ! and PIPESTATUS see above
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+	# e.g. return value is 1
+	#  then getopt has complained about wrong arguments to stdout
+	exit 2
+fi
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+msg=""
+addr=""
+# now enjoy the options in order and nicely split until we see --
+while true; do
+	case "$1" in
+		-m|--message)
+			msg="$2"
+			shift 2
+			;;
+		-a|--address)
+			addr="$2"
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Programming error $1 $2"
+			exit 3
+			;;
+	esac
+done
+
+if [[ -z "${addr}" && -z "${msg}" ]];
 then
-	address=$(echo "$line" | head -n1 | awk '{print $1}') # grab the first word of the first line
-	msg=$(echo "$line" | head -n1 | awk '{for(i=2;i<=NF;i++) print $i}') # grab the rest of the line
-	b64=$(echo "$msg" | base64)
-	echo $(curl -sG --data-urlencode "to=$address" --data-urlencode "msg=$b64" http://83.212.127.188:8080)
-fi < "${1:-/dev/stdin}"
-# The substitution ${1:-...} takes $1 if defined
-# otherwise the file name of the standard input of the own process is used.
-# https://stackoverflow.com/a/7045517
+	echo "$0: Missing parameters -m <message> and -a <address>"
+	exit 5
+fi
+if [[ -z "${addr}" ]];
+then
+	addr="$(cat /dev/stdin)"
+fi
+if [[ -z "${msg}" ]];
+then
+	msg="$(cat /dev/stdin)"
+fi
+if [[ -z "${addr}" ]];
+then
+	echo "$0: Missing parameter -a <address>"
+fi
+if [[ -z "${msg}" ]];
+then
+	echo "$0: Missing parameter -m <message>"
+fi
+
+
+#echo "addr:$addr"
+#echo "msg:$msg"
+
+echo $(curl -sG --data-urlencode "to=$addr" --data-urlencode "msg=$(echo $msg | base64)" http://83.212.127.188:8080)
